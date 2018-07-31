472aba67df7600142a06b858978d31d6a21abd9d
Flavio Silva <flaviomau@yahoo.com>
2018-07-31 00:36:09 -0300

    Started use of jsdoc

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,67 @@
+/** Express router providing user related routes
+ * @module users
+ */
+
 const createRouter = (UserModel)=> {
 	const	express         = require('express'),
   			router					= express.Router(),        
         UserController	= require('../controllers/UserController')(UserModel),
         middlewareAuth	= require('../controllers/middlewareAuth')
 
-	router.get('/',             middlewareAuth({route: 11}), UserController.readAll.bind(UserController))
-	router.post('/',            middlewareAuth({route: 12}), UserController.create.bind(UserController))
-	router.put('/:_id',         middlewareAuth({route: 13}), UserController.update.bind(UserController))
-	router.delete('/:_id',      middlewareAuth({route: 14}), UserController.delete.bind(UserController))
-	router.post('/authenticate',                             UserController.authenticate.bind(UserController))
+	/**
+ 	* Route to retrieve users list. Only avaiable to Admin users.
+ 	* @name get/user
+ 	* @function
+ 	* @memberof module:users
+ 	* @inner
+ 	* @returns {Array.<Object>} List of users registered.
+ 	*/
+	router.get('/', middlewareAuth({route: 11}), UserController.readAll.bind(UserController))
+
+	/**
+ 	* Route to insert new users. Only avaiable to Admin users.
+ 	* @name post/user
+ 	* @function
+ 	* @memberof module:users
+ 	* @inner
+	* @param {Object} User data 
+ 	* @returns {Object} Status of operation.
+ 	*/
+	router.post('/', middlewareAuth({route: 12}), UserController.create.bind(UserController))
+
+	/**
+ 	* Route to update one specific user. Only avaiable to Admin	 users.
+ 	* @name put/user
+ 	* @function
+ 	* @memberof module:users
+	* @inner
+	* @param {String} Id User id 
+	* @param {Object} Data User data 
+ 	* @returns {Object} Status of operation.
+ 	*/
+	router.put('/:_id', middlewareAuth({route: 13}), UserController.update.bind(UserController))
+
+	/**
+ 	* Route to delete one specific user. Only avaiable to Admin users.
+ 	* @name delete/user
+ 	* @function
+ 	* @memberof module:users
+	* @inner
+	* @param {String} User id
+ 	* @returns {Object} Status of operation.
+ 	*/
+	router.delete('/:_id', middlewareAuth({route: 14}), UserController.delete.bind(UserController))
+
+	/**
+ 	* Route to retrieve users list. Only avaiable to Admin users.
+ 	* @name post/authenticate
+ 	* @function
+ 	* @memberof module:users
+	* @inner
+	* @param {Object} User data - username and password.
+ 	* @returns {Object} Status of operation - token included.
+ 	*/
+	router.post('/authenticate', UserController.authenticate.bind(UserController))
 	return router
 }
 
